b46b3bfb8ed7bbcd33f597b1abcb677d5839198c
PSILVA999 <p.silva0399@gmail.com>
2022-05-24 16:01:02 -0300

    Adicionei as funcionalidades do menu

---
+++ src/Components/Events/handleMenu.js
@@ -0,0 +1,58 @@
+export function resizeWindow() {
+  if (window.matchMedia("(min-width:1041px)").matches) {
+    document.querySelector(".menu").classList.remove('active')
+    document.querySelector(".login-menu").classList.remove("active")
+  }
+}
+
+export function menuMobile(e) {
+  console.log(e.target, e.currentTarget)
+
+  let toggleMenu = document.querySelector(".toggle-menu"),
+      menu = document.querySelector(".menu"),
+      buttonLoginMenu = document.querySelector(".login-menu")
+
+  let tempo = 3
+
+  if (menu.classList.contains('active')) {
+    menu.style.animation = 'hides-menu .3s forwards'
+    toggleMenu.classList.remove("close")
+
+    let retiraMenu = setInterval(timeMenu, 100)
+    function timeMenu() {
+      tempo--
+
+      if (tempo === 0) {
+        menu.classList.remove('active')
+        menu.style.animation = 'show-menu .3s forwards'
+        clearInterval(retiraMenu)
+      }
+    }
+  }
+
+  else {
+    toggleMenu.classList.add("close")
+    menu.classList.add('active')
+    buttonLoginMenu.classList.add('active')
+  }
+}
+
+export function closeMenu(e) {
+  let tempo = 3
+
+  if (e.target.id !== 'toggle-menu' && e.target.id !== 'menu') {
+    document.querySelector('.menu').style.animation = 'hides-menu .3s forwards'
+    document.querySelector('.toggle-menu').classList.remove("close")
+
+    let retiraMenu = setInterval(timeMenu, 100)
+    function timeMenu() {
+      tempo--
+
+      if (tempo === 0) {
+        document.querySelector('.menu').classList.remove('active')
+        document.querySelector('.menu').style.animation = 'show-menu .3s forwards'
+        clearInterval(retiraMenu)
+      }
+    }
+  }
+}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { resizeWindow, menuMobile, closeMenu } from './Events/handleMenu.js'
+
 import { ReactComponent as Logo } from '../Assets/0-logo.svg'
 import { ReactComponent as Arrow } from '../Assets/1-arrow.svg'
 
@@ -6,19 +8,22 @@ import './scss/header.scss'
 import { showFirstDetail, hidesFirstDetail, showSecondDetail, hidesSecondDetail, showThirdDetail, hidesThirdDetail } from './Events/handleDetail'
 
 const Header = () => {
+  window.addEventListener('resize', resizeWindow)
+  document.body.addEventListener('click', closeMenu)
+
   return (
     <header>
       <Logo className='logo'/>
 
       <nav>
-        <button className='toggle-menu'></button>
+        <button className='toggle-menu' onClick={ menuMobile } id='toggle-menu'></button>
 
-        <ul className='menu active'>
+        <ul className='menu' id='menu'>
           <li onMouseOver={ showFirstDetail } onMouseOut={ hidesFirstDetail }>História</li>
           <li onMouseOver={ showSecondDetail } onMouseOut={ hidesSecondDetail }>Recordes</li>
           <li onMouseOver={ showThirdDetail } onMouseOut={ hidesThirdDetail }>Contato</li>
 
-          <button className='login-menu active'>
+          <button className='login-menu'>
             LOGIN 
             <Arrow/>
           </button>
